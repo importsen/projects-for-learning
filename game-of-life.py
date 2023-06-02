8ae164485dbfab516fb7c0cd9fd8ca0303b55425
sen <senecayung@gmail.com>
2023-06-02 12:14:41 -0400

    refactoring

--- game-of-life.py
+++ game-of-life.py
@@ -8,24 +8,23 @@ class GameOfLife:
         self.grid = np.zeros((rows, columns), dtype=int)
         self.simulation_running = False
 
-    def update_grid(self):
-        global grid 
-        new_grid = grid.copy() 
+    def update_grid(self): 
+        new_grid = self.grid.copy() 
 
-        for i in range(rows):
-            for j in range(columns):
+        for i in range(self.rows):
+            for j in range(self.columns):
                 #calculate the number of neighbors
-                neighbors = np.sum(grid[max(0, i-1):min(i+2, rows), max(0, j-1):min(j+2, columns)]) - grid[i, j]
+                neighbors = np.sum(self.grid[max(0, i-1):min(i+2, self.rows), max(0, j-1):min(j+2, self.columns)]) - self.grid[i, j]
 
             # apply the rules of the game 
-                if grid[i, j] == 1:
+                if self.grid[i, j] == 1:
                     if neighbors < 2 or neighbors > 3:
                         new_grid[i, j] = 0
                 else:
                     if neighbors == 3:
                         new_grid[i, j] = 1
     
-        grid = new_grid
+        self.grid = new_grid
 
     def toggle_cell(self, row, col):
         # toggle the cell at specified coordinates
@@ -35,7 +34,7 @@ class GameOfLife:
         for row in self.grid:
             print(' '.join(["*" if cell == 1 else "." for cell in row]))
 
-    def handle_user_input():
+    def handle_user_input(self):
         while True:
             user_input = input("Enter the cell coordinates (row columns) to toggle (e.g.,2 3). 'exit' to quit: ")
             if user_input == "exit":
@@ -43,7 +42,7 @@ class GameOfLife:
 
             try:
                 row, col = map(int, user_input.split())
-                if 0 <= row < rows and 0 <= col < columns:
+                if 0 <= row < self.rows and 0 <= col < self.columns:
                     self.toggle_cell(row, col)
                 else:
                     print("Invalid input! Please enter valid coordinates withinthe grid range")
@@ -52,9 +51,9 @@ class GameOfLife:
 
             user_command= input("Enter a command ('start, 'pause', 'reset'): ")
             if user_command == "start":
-                simulation_running = True
+                self.simulation_running = True
             elif user_command == "pause":
-                simulation_running = False
+                self.simulation_running = False
             elif user_command == "reset":
                 self.grid = np.zeros((self.rows, self.columns), dtype=int)
             else:
