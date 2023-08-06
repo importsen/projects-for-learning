957226104adbe1072490795e609a6452dc22c6e3
sen <senecayung@gmail.com>
2023-08-06 04:29:52 -0400

    new folder: 'leetcode'

---
+++ leetcode/1768-merge-strings-alternately.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mergeAlternatively(self, word1: str, word2: str) -> str:
+        i, j, result = 0, 0, ""
+        while i < len(word1) and j < len(word2):
+            result += word1[i] + word2[j]
+            i += 1 
+            j += 2 
+        return result + word1[i:] + word2[j:]
+
+solution = Solution()
+        
+merged_string = solution.mergeAlternatively("hello", "world")
+
+print(merged_string)
---
+++ practice-python-50/problem-5.py
@@ -0,0 +1,23 @@
+"""
+take 2 lists and write a program that returns a list that contains only the 
+elements that are common between the lists (no doups). Make sure the program 
+works with two lists of different sizes.
+
+Extras:
+1. ramdom gen two lists to test this
+2, write this on one line of python
+"""
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+c = []
+
+for i in b:
+    if a[0] == b[0]:
+
+        c.append
+
+print(c)
+
